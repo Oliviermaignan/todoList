7ad8f0abeac582c93a13ecc8213752dd1af453a4
Olivier <ol.maignan@gmail.com>
2024-03-18 19:11:42 +0100

    fin de register

--- src/treatment/registerTreatment.php
+++ src/treatment/registerTreatment.php
@@ -5,6 +5,28 @@ if (!empty(file_get_contents('php://input'))) {
 
     $json=json_decode(file_get_contents('php://input'));
 
+    // Sanitize all my data
+    $sanitizedObj = [
+        "nom" => htmlspecialchars($json->nom),
+        "prenom" => htmlspecialchars($json->prenom),
+        "email" => filter_var($json->email, FILTER_SANITIZE_EMAIL),
+        "password" => password_hash($json->password, PASSWORD_DEFAULT)
+    ];
+
+    //instance Users
+    $user = new Users(
+        $sanitizedObj["nom"],
+        $sanitizedObj["prenom"],
+        $sanitizedObj["email"],
+        $sanitizedObj["password"]
+    );
+    var_dump($user);
+
+    // Create user
+    $userRepo = new UsersRepository();
+    $creationNewUser = $userRepo->create($user);
+
+    var_dump($creationNewUser);
     echo json_encode($json);
 }
 
